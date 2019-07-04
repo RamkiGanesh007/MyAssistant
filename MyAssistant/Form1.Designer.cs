e44df873ff81d1c6675bb4f6d6b6215390c73991
ganesh kumar <gk4309303@gmail.com>
2019-07-04 22:59:17 +0530

    Button Colour Change Successful

--- MyAssistant/Form1.Designer.cs
+++ MyAssistant/Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Rough = new System.Windows.Forms.Button();
+            this.QA = new System.Windows.Forms.Button();
+            this.Projects = new System.Windows.Forms.Button();
+            this.MyProgr = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -39,7 +42,7 @@
             this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.richTextBox1.EnableAutoDragDrop = true;
             this.richTextBox1.ForeColor = System.Drawing.Color.White;
-            this.richTextBox1.Location = new System.Drawing.Point(-1, 51);
+            this.richTextBox1.Location = new System.Drawing.Point(1, 50);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
             this.richTextBox1.Size = new System.Drawing.Size(1144, 550);
@@ -47,16 +50,53 @@
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
-            // button1
+            // Rough
             // 
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(0, 1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 49);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Rough_Work";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Rough.BackColor = System.Drawing.Color.White;
+            this.Rough.ForeColor = System.Drawing.Color.Black;
+            this.Rough.Location = new System.Drawing.Point(-1, 1);
+            this.Rough.Name = "Rough";
+            this.Rough.Size = new System.Drawing.Size(169, 49);
+            this.Rough.TabIndex = 1;
+            this.Rough.Text = "Rough_Work";
+            this.Rough.UseVisualStyleBackColor = false;
+            this.Rough.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // QA
+            // 
+            this.QA.BackColor = System.Drawing.Color.White;
+            this.QA.ForeColor = System.Drawing.Color.Black;
+            this.QA.Location = new System.Drawing.Point(169, 1);
+            this.QA.Name = "QA";
+            this.QA.Size = new System.Drawing.Size(169, 49);
+            this.QA.TabIndex = 2;
+            this.QA.Text = "QA\'s";
+            this.QA.UseVisualStyleBackColor = false;
+            this.QA.Click += new System.EventHandler(this.QA_Click);
+            // 
+            // Projects
+            // 
+            this.Projects.BackColor = System.Drawing.Color.White;
+            this.Projects.ForeColor = System.Drawing.Color.Black;
+            this.Projects.Location = new System.Drawing.Point(338, 1);
+            this.Projects.Name = "Projects";
+            this.Projects.Size = new System.Drawing.Size(169, 49);
+            this.Projects.TabIndex = 3;
+            this.Projects.Text = "Projects";
+            this.Projects.UseVisualStyleBackColor = false;
+            this.Projects.Click += new System.EventHandler(this.Projects_Click);
+            // 
+            // MyProgr
+            // 
+            this.MyProgr.BackColor = System.Drawing.Color.White;
+            this.MyProgr.ForeColor = System.Drawing.Color.Black;
+            this.MyProgr.Location = new System.Drawing.Point(507, 1);
+            this.MyProgr.Name = "MyProgr";
+            this.MyProgr.Size = new System.Drawing.Size(169, 49);
+            this.MyProgr.TabIndex = 4;
+            this.MyProgr.Text = "MyProgress";
+            this.MyProgr.UseVisualStyleBackColor = false;
+            this.MyProgr.Click += new System.EventHandler(this.MyProgr_Click);
             // 
             // MainForm
             // 
@@ -65,7 +105,10 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1144, 601);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyProgr);
+            this.Controls.Add(this.Projects);
+            this.Controls.Add(this.QA);
+            this.Controls.Add(this.Rough);
             this.Controls.Add(this.richTextBox1);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -83,7 +126,10 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Rough;
+        private System.Windows.Forms.Button QA;
+        private System.Windows.Forms.Button Projects;
+        private System.Windows.Forms.Button MyProgr;
     }
 }
 
--- MyAssistant/Form1.cs
+++ MyAssistant/Form1.cs
@@ -19,7 +19,7 @@ namespace MyAssistant
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-                
+           
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -27,15 +27,64 @@ namespace MyAssistant
 
         }
 
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.button1.BackColor = Color.Black;
-            this.button1.ForeColor =Color.White ;
+            if (Rough.BackColor == Color.Black || QA.BackColor == Color.Black || Projects.BackColor == Color.Black || MyProgr.BackColor == Color.Black)
+            {
+                this.Rough.BackColor = Color.White;
+                this.Rough.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.Rough.BackColor = Color.Black;
+                this.Rough.ForeColor = Color.White;
+            }
+        }
+
+        private void QA_Click(object sender, EventArgs e)
+        {
+            if (Rough.BackColor == Color.Black || QA.BackColor == Color.Black || Projects.BackColor == Color.Black || MyProgr.BackColor == Color.Black)
+            {
+                this.QA.BackColor = Color.White;
+                this.QA.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.QA.BackColor = Color.Black;
+                this.QA.ForeColor = Color.White;
+            }
+
+        }
+
+        private void Projects_Click(object sender, EventArgs e)
+        {
+            if (Rough.BackColor == Color.Black || QA.BackColor == Color.Black || Projects.BackColor == Color.Black || MyProgr.BackColor == Color.Black)
+            {
+                this.Projects.BackColor = Color.White;
+                this.Projects.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.Projects.BackColor = Color.Black;
+                this.Projects.ForeColor = Color.White;
+            }
+        }
+
+        private void MyProgr_Click(object sender, EventArgs e)
+        {
+            if (Rough.BackColor == Color.Black || QA.BackColor == Color.Black || Projects.BackColor == Color.Black || MyProgr.BackColor == Color.Black)
+            {
+                this.MyProgr.BackColor = Color.White;
+                this.MyProgr.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.MyProgr.BackColor = Color.Black;
+                this.MyProgr.ForeColor = Color.White;
+            }
+           
         }
     }
 }
